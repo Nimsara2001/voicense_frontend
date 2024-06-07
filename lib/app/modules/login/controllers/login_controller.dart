8652c88f709cf2ab864bd174b4eca45e773e8ac3
Hansaka2001 <hansakarukshan43@gmail.com>
2024-06-08 00:24:31 +0530

    login changes

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -2,10 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
-import 'package:voicense_frontend/app/data/user_account_storage.dart';
-import 'package:voicense_frontend/app/modules/home/views/home_view.dart';
 import 'dart:convert';
-import 'package:voicense_frontend/app/modules/stu_home/views/stu_home_view.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginController extends GetxController {
   final GlobalKey<FormState> loginFormKey = GlobalKey<FormState>();
@@ -13,7 +11,8 @@ class LoginController extends GetxController {
   var isLoading = false.obs;
   String? username;
   String? password;
-  final UserAccountStorage _userAccountStorage = Get.put(UserAccountStorage());
+  String? serverUsernameError;
+  String? serverPasswordError;
 
   @override
   void onInit() {
@@ -33,12 +32,18 @@ class LoginController extends GetxController {
     if (value.isEmpty) {
       return "Please enter your username";
     }
+    if (serverUsernameError != null) {
+      return serverUsernameError;
+    }
     return null;
   }
 
   String? validatePassword(String value) {
     if (value.isEmpty) {
       return "Please enter your password";
+    }
+    if (serverPasswordError != null) {
+      return serverPasswordError;
     }
     return null;
   }
@@ -59,6 +64,7 @@ class LoginController extends GetxController {
         'An error occurred during authentication',
         snackPosition: SnackPosition.BOTTOM,
       );
+
       print(e);
     } finally {
       isLoading.value = false;
@@ -67,7 +73,7 @@ class LoginController extends GetxController {
 
   Future<void> login(String username, String password) async {
     final response = await http.post(
-      Uri.parse('http://192.168.8.100/auth/login'),
+      Uri.parse('http://192.168.8.111/auth/login'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
@@ -77,13 +83,32 @@ class LoginController extends GetxController {
       }),
     );
 
+    final responseData = jsonDecode(response.body);
+    if (responseData['message'] == 'invalid' &&
+        responseData['reason'] == 'incorrect_username') {
+      serverUsernameError = 'Incorrect username';
+    }
+    if (responseData['message'] == 'invalid' &&
+        responseData['reason'] == 'incorrect_password') {
+      serverPasswordError = 'Incorrect password';
+    }
     if (response.statusCode == 200) {
       // If the server returns a 200 OK response, parse the JSON.
       Map<String, dynamic> jsonResponse = jsonDecode(response.body);
       print(jsonResponse);
+
+      if (jsonResponse['message'] == 'success') {
+        String token = jsonResponse['token'];
+        await storeToken(token);
+      }
     } else {
       // If the server returns an error response, throw an exception.
       throw Exception('Failed to login.');
     }
+  }
+
+  Future<void> storeToken(String token) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('token', token);
   }
 }
