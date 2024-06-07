b3559f91829348939093cc2068a3f8caa6aeb2de
SudeeraDilshan <new1818155@gmail.com>
2024-06-07 22:51:18 +0530

    pop up menu

--- lib/app/widgets/popup_menu_btn.dart
+++ lib/app/widgets/popup_menu_btn.dart
@@ -1,27 +1,119 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 
 class PopupMenuBtnController extends GetxController {
+  String note_id;
+  PopupMenuBtnController(this.note_id);
 
-  RxString value = ''.obs;
+ Future<void> sendRequest(String action) async {
+   try{
+    // Depending on the action, you can modify the request
+    if (action == 'share') {
+      final String url = 'http://192.168.8.100:8000/note/share/$note_id';
+      // Send a request to share the item
+      final response = await http.post(
+        Uri.parse(url),
+        body: {'action': 'share'},
+      );
 
+      if (response.statusCode == 200) {
+        print('Item shared successfully');
+        //put snack messege 
+      } else {
+        print('Failed to share item');
+        //put snack messege 
+      }
+    } else if (action == 'trash') {
+      // Send a request to delete the item
+      final String url = 'http://192.168.8.100:8000/note/trash/$note_id';
+      final response = await http.post(
+        Uri.parse(url),
+        body: {'action': 'trash'},
+      );
+
+      if (response.statusCode == 200) {
+        print('Item deleted successfully');
+        //put snack messege 
+      } else {
+        print('Failed to delete item');
+        //put snack messege 
+         Get.snackbar('Error', 'Error during trash: ${response.body}',
+          snackPosition: SnackPosition.BOTTOM);
+      }
+    }
+  }
+  catch(e){
+       print('Error : $e');
+      Get.snackbar('Error', 'Error during proccessed: $e',
+          snackPosition: SnackPosition.BOTTOM);
+  }
+ }
+
+  void _handleMenuSelection(BuildContext context, String choice) {
+    if (choice == 'Share') {
+      _showAlertDialog(context, 'Share', 'Do you want to share this item?');
+    } else if (choice == 'Trash') {
+      _showAlertDialog(context, 'Trash', 'Do you want to delete this item?');
+    }
+  }
+
+
+  Future<void> _showAlertDialog(BuildContext context, String title, String content) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text(content),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Yes'),
+              onPressed: () async {
+                await sendRequest(title.toLowerCase());
+                Get.back();
+              },
+            ),
+            TextButton(
+              child: const Text('No'),
+              onPressed: () {
+                Get.back();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+  
 }
+
+
+
+
+
+
+
 class PopupMenuBtn extends GetView<PopupMenuBtnController> {
   String note_id;
   PopupMenuBtn(this.note_id);
 
   @override
   Widget build(BuildContext context) {
-    final PopupMenuBtnController _controllerX = Get.put(PopupMenuBtnController());
+    final PopupMenuBtnController _controllerX = Get.put(PopupMenuBtnController(note_id));
 
     return PopupMenuButton<String>(
                       onSelected: (String choice) {
-                        
-                        if (choice == 'Share') {
-                          // Handle share functionality
-                        } else if (choice == 'Trash') {
-                          
-                        }
+                        // Handle share functionality
+                        _controllerX._handleMenuSelection(context, choice);
+
                       },
                       itemBuilder: (BuildContext context) =>
                           <PopupMenuEntry<String>>[
